b676bcb88a497f40e3f7dcf1be0322f657dd7ddb
svintuskbd <swintuskbd@gmail.com>
2015-11-05 00:06:21 +0200

    add view all and view sigle post

--- src/Oleg/TestBundle/Controller/PortfolioController.php
+++ src/Oleg/TestBundle/Controller/PortfolioController.php
@@ -11,17 +11,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class PortfolioController extends Controller 
-{
+{    
     public function portfolioAction() 
     {
-        $arr = array('portfolio' => array (
-            0 => 'title1',
-            1 => 'title2',
-            2 => 'title3',
-            3 => 'title4',
-            4 => 'title5',
-        ));
+        $em = $this->get('doctrine.orm.entity_manager');
+        $data = $em->getRepository('OlegTestBundle:Post')->findAll();
+        $arr = array('portfolio' => $data);
         return $this->render('OlegTestBundle::portfolio_view.html.twig', $arr);
+    }
+    
+    public function viewAction($id)
+    {
+        $url = 'http://128.199.53.5/recipes?page=1&per_page=1&api_key=33608dcc73018d51a896cfb5d1e386f8';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $content = json_decode($response, true);
+//        dump($content);
+        
+        
+        
+        $em = $this->get('doctrine.orm.entity_manager');
+        $data = $em->getRepository('OlegTestBundle:Post')->find($id);
+        $arr = array('portfolio' => $data, 'content' => $content);
+        return $this->render('OlegTestBundle::single_view.html.twig', $arr);
+    }
+    
+    public function delAction($id)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $data = $em->getRepository('OlegTestBundle:Post')->find($id);
+        try {
+            $em->remove($data);
+            $em->flush();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            $product = null;
+        }
+        
+        return $this->redirectToRoute('portfolio');
     }
     
 }
---
+++ src/Oleg/TestBundle/Entity/Post.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Oleg\TestBundle\Entity;
+
+/**
+ * Post
+ */
+class Post
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Post
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Post
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Post
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
+
